6e3f0304f322e7d2af4bac9030ebbb36e0dd5cdc
crimmzx <111248873+crimmzx@users.noreply.github.com>
2022-12-12 00:32:25 -0500

    commit partial final

--- src/decTimelineElements.js
+++ src/decTimelineElements.js
@@ -29,6 +29,36 @@ let decTimelineElements = [
         date: "12/05",
         icon: "study",
       },
+      {
+        id: 6,
+        title: "Studying at Icahn for Math",
+        mood: "Tired",
+        location: "Aliya Kanji", 
+        image: "pictures/12_06.jpg",
+        description: "Studying with friends to prep for Math exam the following morning.",
+        date: "12/06",
+        icon: "study",
+      },
+      {
+        id: 7,
+        title: "Walking to Aliya Kanji Hall",
+        mood: "Tired",
+        location: "Aliya Kanji", 
+        image: "pictures/12_07.jpg",
+        description: "Walking to Aliya Kanji Hall to continue studying for the night.",
+        date: "12/07",
+        icon: "study",
+      },
+      {
+        id: 9,
+        title: "BodyHype's show: MIRAGE",
+        mood: "Excited",
+        location: "Frist", 
+        image: "pictures/12_09.jpg",
+        description: "Saw BodyHype's show! It was so fun and so mind-blowing to see what dancers can do!",
+        date: "12/09",
+        icon: "friends",
+      },
   ];
   
 export default decTimelineElements;
